4c777804b84ebb03c8bd6f12d5e6cb367f720260
Eduardo Rodriguez T <edtorrestorija@Mac-mini-de-Eduardo.local>
2018-02-13 21:18:07 -0600

    Finished week1 assignment

--- Algorithmic toolbox/week1_programming_challenges/mine/max_pairwise.py
+++ Algorithmic toolbox/week1_programming_challenges/mine/max_pairwise.py
@@ -4,11 +4,18 @@ a = [int(x) for x in input().split()]
 assert(len(a) == n)
 
 result = 0
-max1 = a[0]
-max2 = a[1]
-for i in range(0, n):
-    if a[i] > max1:
-        max2 = max1
-        max1 = a[i]
+max1 = 0
+max2 = 0
+if n == 2:
+    max1 = a[0]
+    max2 = a[1]
+else:
+    for i in range(0, n):
+        if a[i] > max1:
+            max2 = max1
+            max1 = a[i]
+        elif a[i] > max2:
+            max2 = a[i]
+
 result = max1 * max2
 print(result)
